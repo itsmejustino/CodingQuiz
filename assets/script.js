5c01c0f38d598c5a66413be78b05500f29415f65
jdog <thuz3rfr13ndly@gmail.com>
2022-05-09 15:13:43 -0600

    create question page delete uneeded pages

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,30 @@
+var questions = 0;
+var score = 0;
+
+for (var i = 0; i < questions.length; i++) {
+    let response = window.prompt(questions[i].prompt(questions))
+
+    if(response == questions[i].answer){
+    score++;
+    alert("Nice job!")}
+    else{
+
+        alert("Wrong answer!")
+    }
+
+}
+
+
+//quiz start function
 var startQuiz = document.querySelector('btn-primary')
 
-startQuiz.addEventListener('click', function() {
+
+startQuiz.addEventListener('click', function(
+     
+    {
 
     
 
 
-})
+});
+)
