3f8af523fefb9917b062d4747dc1b8b1e7487ed2
jdog <thuz3rfr13ndly@gmail.com>
2022-05-13 13:51:36 -0600

    add highscore page and js to get local score

--- assets/script-questions.js
+++ assets/script-questions.js
@@ -1,15 +1,31 @@
 //Timer
 var timerEL = document.getElementById("time-left");
-var countdown = 15;
 
-var startTimer = setInterval(function () {
+var Counter = document.getElementById("question-counter-up");
+var totalQuestionCounter = document.getElementById("question-counter-left");
+var nextButton = document.getElementById("next-button");
+var questionCounter = 1;
+
+var countdown = 60;
+var startTimer = setInterval(() => {
+  
   countdown--;
   timerEL.textContent = countdown;
+  if(questionCounter == totalQuestionCounter.textContent)
+  {
+    nextButton.innerText = "End Quiz"
+    nextButton.addEventListener("click",endQuiz);}
+
   if (countdown <= 0) {
     clearInterval(startTimer);
+    endQuiz();
   }
   if (countdown <= 5) {
     timerEL.setAttribute("style", "color: red;");
+  }
+
+  if (countdown >= 6) {
+    timerEL.setAttribute("style", "color: green;");
   }
 }, 1000);
 
@@ -21,53 +37,78 @@ if (countdown == 0) {
 
 //question tracker
 
-var Counter = document.getElementById("question-counter-up");
-var totalQuestionCounter = document.getElementById("question-counter-left");
-var nextButton = document.getElementById("next-button");
-
 totalQuestionCounter.textContent = 4;
-var questionCounter = 1;
+
 Counter.textContent = questionCounter;
+
+autoContinue = () => {
+  questionCounter++;
+  currentQuestionsIndex++;
+  currentAnswersIndex++;
+  Counter.textContent = questionCounter;
+  displayQuestion();
+  displayAnswers();
+}
 
 //function executes on click of next question button.
 //question counter increases by 1, showNextQuestion, and showAnswers execute on click.
-function questionTracker() {
+
+questionTracker = () => {
   nextButton.addEventListener("click", () => {
+    if (questionCounter == totalQuestionCounter.textContent) {
+      return;
+    }
     questionCounter++;
     currentQuestionsIndex++;
     currentAnswersIndex++;
     Counter.textContent = questionCounter;
-    countdown = 15;
-    console.log(questionCounter);
-    console.log(currentAnswersIndex);
-    console.log(currentAnswersIndex);
     displayQuestion();
     displayAnswers();
   });
-}
+};
 
-function endQuiz() {}
-
-//start game function
-var questionEl = document.getElementById("question");
-var answerButton1 = document.getElementById("btn-btn1");
-var answerButton2 = document.getElementById("btn-btn2");
-var answerButton3 = document.getElementById("btn-btn3");
-var answerButton4 = document.getElementById("btn-btn4");
-
+//Quiz Start function
 var randomQuestions;
 let currentQuestionsIndex = 0;
 let currentAnswersIndex = 0;
 
 function startQuiz() {
   console.log("Quiz started");
-
   currentQuestionsIndex;
   displayQuestion();
   displayAnswers();
   questionTracker();
   chooseAnswer();
 }
+
+//Quiz end function
+
+
+
+function endQuiz() {
+  
+    window.location.href= '../pages/page3.html';
+  clearTimeout(startTimer);
+  
+}
+
+//start game function
+
+function nextQuestion(){
+  questionCounter++;
+    currentQuestionsIndex++;
+    currentAnswersIndex++;
+    Counter.textContent = questionCounter;
+    displayQuestion();
+    displayAnswers();
+  
+}
+var questionEl = document.getElementById("question");
+var answerButton1 = document.getElementById("btn-btn1");
+var answerButton2 = document.getElementById("btn-btn2");
+var answerButton3 = document.getElementById("btn-btn3");
+var answerButton4 = document.getElementById("btn-btn4");
+
 
 function displayQuestion() {
   questionEl.innerText = questions[currentQuestionsIndex].question;
@@ -92,33 +133,51 @@ function chooseAnswer() {
 function checkAnswer1() {
   if (answerButton1.innerText == questions[currentAnswersIndex].correctAnswer) {
     console.log("true!");
+    score++;
+    nextQuestion();
+    console.log("score: " + score);
   } else {
+    countdown -= 4;
     console.log("false");
+    console.log("score: " + score);
   }
 }
 
 function checkAnswer2() {
   if (answerButton2.innerText == questions[currentAnswersIndex].correctAnswer) {
     console.log("true!");
+    score++;
+    nextQuestion();
+    console.log("score: " + score);
   } else {
+    countdown -= 4;
     console.log("false");
+    console.log("score: " + score);
   }
 }
 function checkAnswer3() {
   if (answerButton3.innerText == questions[currentAnswersIndex].correctAnswer) {
     console.log("true!");
+    score++;
+    nextQuestion();
+    console.log("score: " + score);
   } else {
+    countdown -= 4;
     console.log("false");
+    console.log("score: " + score);
   }
-
 }
 function checkAnswer4() {
   if (answerButton4.innerText == questions[currentAnswersIndex].correctAnswer) {
     console.log("true!");
+    score++;
+    nextQuestion();
+    console.log("score: " + score);
   } else {
+    countdown -= 4;
     console.log("false");
+    console.log("score: " + score);
   }
-
 }
 
 //question and answer bank
